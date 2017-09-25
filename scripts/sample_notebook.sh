2c820d0f4d25c3b1f90db6e7b99b312c9b9632ac
fbarilla <fbarilla@us.ibm.com>
2017-09-25 08:58:53 -0500

    Update sample_notebook.sh

--- scripts/sample_notebook.sh
+++ scripts/sample_notebook.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ ! -d /demos]; then
-    cp -a /usr/local/samples /demos
-fi
+# if [ ! -d /demos]; then
+#     cp -a /usr/local/samples /demos
+# fi
 
 exec /usr/local/bin/nimbix_notebook "$@"
